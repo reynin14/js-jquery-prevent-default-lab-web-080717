b8139cefc5d537452504deebb60c58d7c793ffcd
github username <github email address>
2017-09-24 16:44:39 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm(){
+  $('form').on('submit', function(e){
+    var listItem = $('#item').val();
+    $('ol').append('<li>' + listItem + '</li>');
+    e.preventDefault();
+  })
+}
